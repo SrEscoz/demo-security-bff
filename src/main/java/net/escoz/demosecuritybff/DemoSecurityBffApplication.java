0059cd8043e3ae26e44d862d6d1009a4a43f286c
Miguel Escoz <miguel.escoz@nter.es>
2024-12-17 11:06:52 +0100

    Configuración inicial

---
+++ src/main/java/net/escoz/demosecuritybff/DemoSecurityBffApplication.java
@@ -0,0 +1,13 @@
+package net.escoz.demosecuritybff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSecurityBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSecurityBffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/escoz/demosecuritybff/DemoSecurityBffApplicationTests.java
@@ -0,0 +1,13 @@
+package net.escoz.demosecuritybff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSecurityBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
