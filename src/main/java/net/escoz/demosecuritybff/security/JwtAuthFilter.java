4204cecb783241a345b97035b0225835a6d20fe5
Miguel Escoz <miguel.escoz@nter.es>
2024-12-17 14:16:51 +0100

    [FIX] import travieso

--- src/main/java/net/escoz/demosecuritybff/security/JwtAuthFilter.java
+++ src/main/java/net/escoz/demosecuritybff/security/JwtAuthFilter.java
@@ -6,7 +6,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.lang.NonNull;
